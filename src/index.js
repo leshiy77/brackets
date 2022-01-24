5c1a93d0ffa1ec979baefc6b203198fab4f89cf9
leshiy77 <rifbenya@gmail.com>
2022-01-24 16:45:31 +0200

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('');
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strArr.join('').indexOf(bracketsConfig[j].join('')) !== -1) {
+        strArr.splice(strArr.join('').indexOf(bracketsConfig[j].join('')), 2);
+      }
+    }
+  }
+  return strArr.length === 0;
 }
